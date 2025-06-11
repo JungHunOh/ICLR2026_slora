c84557a3b76978f28b67ddb500865dcd5d0dd97e
junghunoh <junghunoh@github.com>
2025-06-11 20:24:07 +0900

    init

--- test.py
+++ test.py
@@ -18,14 +18,18 @@ lr= 0.0002
 
 if dataset == 'commonsense_170k':
     lora = sorted(glob.glob(f'./experiment/{model}_{dataset}_lora_r{rank}_lr{lr}_*result.txt'))
-    slora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora_r{rank}_lr{lr}_*result.txt'))
+    #slora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora_r{rank}_lr{lr}_*result.txt'))
     plora = sorted(glob.glob(f'./experiment/{model}_{dataset}_lora+pissa_r{rank}_lr{lr}_*result.txt'))
-    pslora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora+pissa_r{rank}_lr{lr}_*result.txt'))
+    lmclora = sorted(glob.glob(f'./experiment/{model}_{dataset}_loralmc_r{rank}_lr{lr}_*result.txt'))
+    #pslora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora+pissa_r{rank}_lr{lr}_*result.txt'))
+    pos = 2
 else:
     lora = sorted(glob.glob(f'./experiment/{model}_{dataset}_lora_r{rank}_lr{lr}_*.txt'))
-    slora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora_r{rank}_lr{lr}_*.txt'))
+    #slora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora_r{rank}_lr{lr}_*.txt'))
     plora = sorted(glob.glob(f'./experiment/{model}_{dataset}_lora+pissa_r{rank}_lr{lr}_*.txt'))
-    pslora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora+pissa_r{rank}_lr{lr}_*.txt'))
+    lmclora = sorted(glob.glob(f'./experiment/{model}_{dataset}_loralmc_r{rank}_lr{lr}_*.txt'))
+    #pslora = sorted(glob.glob(f'./experiment/{model}_{dataset}_slora+pissa_r{rank}_lr{lr}_*.txt'))
+    pos = 1
 
 print(f'\n{model}_{dataset}_r{rank}_lr{lr}')
 
@@ -35,10 +39,11 @@ lora_results = []
 slora_results = []
 plora_results = []
 pslora_results = []
+lmclora_results = []
 for i in range(len(lora)):
     l = open(lora[i], 'r')
     
-    print(lora[i].split('_')[-2])
+    print(lora[i].split('_')[-pos])
 
     lora_result = round(float(l.readlines()[-1].split(' ')[-1]),3)
     
@@ -48,27 +53,27 @@ for i in range(len(lora)):
 
 print(f'AVG LoRA: {sum(lora_results)/ len(lora_results)}')
 
-print("\n##########SLoRA##########\n")
+# print("\n##########SLoRA##########\n")
 
-for i in range(len(slora)):
-    sl = open(slora[i], 'r')
+# for i in range(len(slora)):
+#     sl = open(slora[i], 'r')
     
-    print(slora[i].split('_')[-2])
+#     print(slora[i].split('_')[-pos])
 
-    slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
+#     slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
     
-    slora_results.append(slora_result)
+#     slora_results.append(slora_result)
 
-    print(f'SLoRA: {slora_result}')
+#     print(f'SLoRA: {slora_result}')
 
-print(f'AVG SLoRA: {sum(slora_results)/ len(slora_results)}')
+# print(f'AVG SLoRA: {sum(slora_results)/ len(slora_results)}')
 
 print("\n##########LoRA+PiSSA##########\n")
 
 for i in range(len(plora)):
     sl = open(plora[i], 'r')
     
-    print(plora[i].split('_')[-2])
+    print(plora[i].split('_')[-pos])
 
     slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
     
@@ -78,17 +83,32 @@ for i in range(len(plora)):
 
 print(f'AVG LoRA+PiSSA: {sum(plora_results)/ len(plora_results)}')
 
-print("\n##########SLoRA+PiSSA##########\n")
+# print("\n##########SLoRA+PiSSA##########\n")
 
-for i in range(len(pslora)):
-    sl = open(pslora[i], 'r')
+# for i in range(len(pslora)):
+#     sl = open(pslora[i], 'r')
     
-    print(pslora[i].split('_')[-2])
+#     print(pslora[i].split('_')[-pos])
+
+#     slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
+    
+#     pslora_results.append(slora_result)
+
+#     print(f'SLoRA+PiSSA: {slora_result}')
+
+# print(f'AVG SLoRA+PiSSA: {sum(pslora_results)/ len(pslora_results)}')
+
+print("\n##########LoRA LMC##########\n")
+
+for i in range(len(lmclora)):
+    sl = open(lmclora[i], 'r')
+    
+    print(lmclora[i].split('_')[-pos])
 
     slora_result = round(float(sl.readlines()[-1].split(' ')[-1]),3)
     
-    pslora_results.append(slora_result)
+    lmclora_results.append(slora_result)
 
     print(f'SLoRA+PiSSA: {slora_result}')
 
-print(f'AVG SLoRA+PiSSA: {sum(pslora_results)/ len(pslora_results)}')
+print(f'AVG SLoRA+PiSSA: {sum(lmclora_results)/ len(lmclora_results)}')
