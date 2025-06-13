889988ee922a5f967ae3507e65a6b9f6e1a98c6d
junghunoh <junghunoh@github.com>
2025-06-14 02:39:26 +0900

    init

--- math_loralmc.py
+++ math_loralmc.py
@@ -7,7 +7,7 @@ gpu=input()
 #model='llama'
 model='llama3'
 
-lr=5e-4
+lr=1e-3
 r=32
 seed=1
 
@@ -18,28 +18,33 @@ elif model == 'llama':
 elif model == 'llama3':
     base_model = 'meta-llama/Meta-Llama-3-8B'
 
-os.system(f'CUDA_VISIBLE_DEVICES={gpu} python3 -m torch.distributed.launch --master_addr localhost --master_port 1231 --nproc_per_node=2 --use_env train_math.py \
-    --model_name_or_path {base_model}\
-    --data_path ft-training_set/MetaMathQA-40K.json \
-    --data_length 10000000 \
-    --bf16 True \
-    --output_dir ./trained_models/{model}_metamath_loralmc_r{r}_lr{lr}_seed{seed}/\
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
-    --evaluation_strategy "no" \
-    --save_strategy "no" \
-    --learning_rate {lr}\
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --logging_steps 1 \
-    --num_train_epochs 2 \
-    --target_modules q_proj k_proj v_proj up_proj down_proj \
-    --lora_r {r}\
-    --lora_alpha {r*2*1.6}\
-    --seed {seed}\
-    --keep_lmc\
-     ')
+for seed in [1]:
+    for r in [8,32,128]:
+        for lr in [2e-4,1e-4,5e-5,2e-5]:
+    os.system(f'CUDA_VISIBLE_DEVICES={gpu} python3 -m torch.distributed.launch --master_addr localhost --master_port 1231 --nproc_per_node=4 --use_env train_math.py \
+        --model_name_or_path {base_model}\
+        --data_path ft-training_set/MetaMathQA-40K.json \
+        --data_length 10000000 \
+        --bf16 True \
+        --output_dir ./trained_models/{model}_metamath_loralmc_r{r}_lr{lr}_seed{seed}/\
+        --per_device_train_batch_size 4 \
+        --per_device_eval_batch_size 4 \
+        --gradient_accumulation_steps 4 \
+        --evaluation_strategy "no" \
+        --save_strategy "no" \
+        --learning_rate {lr}\
+        --weight_decay 0. \
+        --warmup_ratio 0.03 \
+        --logging_steps 1 \
+        --num_train_epochs 3 \
+        --lr_scheduler_type "cosine"\
+        --target_modules q_proj k_proj v_proj up_proj down_proj \
+        --lora_r {r}\
+        --lora_alpha {r*2*1.6}\
+        --seed {seed}\
+        --keep_lmc\
+        ')
 
-os.system(f'python eval_gsm8k.py --model ./trained_models/{model}_metamath_loralmc_r{r}_lr{lr}_seed{seed}/ --data_file ./dataset/GSM8K_test.jsonl')
-os.system(f'python eval_math.py --model ./trained_models/{model}_metamath_loralmc_r{r}_lr{lr}_seed{seed}/ --data_file ./dataset/MATH_test.jsonl')
+    os.system(f'CUDA_VISIBLE_DEVICES={gpu} python eval_gsm8k.py --model ./trained_models/{model}_metamath_loralmc_r{r}_lr{lr}_seed{seed}/ --data_file ./dataset/GSM8K_test.jsonl')
+    #os.system(f'python eval_math.py --model ./trained_models/{model}_metamath_loralmc_r{r}_lr{lr}_seed{seed}/ --data_file ./dataset/MATH_test.jsonl')
+
