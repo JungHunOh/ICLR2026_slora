5c23a9852981e0f0ce62eae92413d86a9477665a
junghunoh <junghunoh@github.com>
2025-06-13 15:02:47 +0900

    init

--- lora_lmc.py
+++ lora_lmc.py
@@ -7,16 +7,19 @@ dataset='commonsense_170k'
 #dataset='math_10k'
 
 #model='gpt'
-model='llama'
+#model='llama'
+model='llama3'
 
-lr=2e-4
-r=4
+lr=1e-4
+r=32
 seed=1
 
 if model == 'gpt':
     base_model = 'EleutherAI/gpt-j-6b'
 elif model == 'llama':
     base_model = 'yahma/llama-7b-hf'
+elif model == 'llama3':
+    base_model = 'meta-llama/Meta-Llama-3-8B'
 
 # math
 os.system(f'CUDA_VISIBLE_DEVICES={gpu} python finetune.py --base_model {base_model} --data_path ./ft-training_set/{dataset}.json --output_dir ./trained_models/{model}_{dataset}_loralmc_r{r}_lr{lr}_seed{seed}/ --batch_size 16 --micro_batch_size 4   --num_epochs 3   --learning_rate {lr}   --cutoff_len 256   --val_set_size 0 --eval_step 80 --save_step 80  --adapter_name lora --lora_r {r} --lora_alpha {r*2} --keep_lmc --seed {seed}')
